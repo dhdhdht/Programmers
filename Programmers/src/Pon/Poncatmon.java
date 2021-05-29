61a119a1e29251ed8f3c351afde9f21e3b62a69e
dhdhdht <breakboy0526@gmail.com>
2021-05-29 17:04:01 +0900

    Programmers

---
+++ Programmers/src/Pon/Poncatmon.java
@@ -0,0 +1,33 @@
+package pon;
+
+import java.util.*;
+
+public class Poncatmon {
+	public static void main(String[] args) {
+		int[] nums = {3, 3, 3, 2, 2, 4};
+		// return 2
+		
+		System.out.println(solution(nums));
+	}
+	
+	    public static int solution(int[] nums) {
+	        int answer = 0;
+	        int choice = nums.length/2;	//¼±ÅÃÇÒ È½¼ö
+	        
+	        Set<Integer> num = new HashSet<Integer>();
+	        
+	        for (int n : nums) {
+	        	num.add(n);
+	        }
+	        //System.out.println(num);
+	        
+	        if (choice < num.size()) {
+	        	answer = choice;
+	        } else {
+	        	answer = num.size();
+	        }
+	        
+	        return answer;
+	    }
+	
+}
